5ecb66ad88fb73b5bb5346703687f233b3524bbe
pirminis <pirminis@gmail.com>
2023-01-28 20:31:02 +0200

    Add main script file

---
+++ torn-city-notify-visual-enhancements.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Visual enhancements for Torn City
+// @namespace    https://github.com/pirminis/torn-city-visual-enhancements
+// @version      0.0.1
+// @description  Visual enhancements for Torn City
+// @author       pirminis
+// @match        https://www.torn.com/loader.php?sid=attack&user2ID=*
+// @updateURL    https://github.com/pirminis/torn-city-visual-enhancements/raw/master/torn-city-visual-enhancements.user.js
+// @downloadURL  https://github.com/pirminis/torn-city-visual-enhancements/raw/master/torn-city-visual-enhancements.user.js
+// ==/UserScript==
+
+(function(global) {
+  'use strict';
+
+  // implement
+})(window);
